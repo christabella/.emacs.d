0138c57e25452c16c1a46c1872c228d6bf4bb5e3
Christabella Irwanto <christabella.irwanto@gmail.com>
2017-09-15 15:29:02 +0800

    oops

--- custom.el
+++ custom.el
@@ -3,10 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(crux-reopen-as-root-mode t)
  '(debug-on-error t)
  '(package-selected-packages
    (quote
-    (crux elpy isend-mode wgrep exec-path-from-shell flycheck projectile yasnippet which-key nyan-mode multiple-cursors magit expand-region counsel company-quickhelp company beacon aggressive-indent ace-window zenburn-theme use-package)))
+    (anaconda-mode crux isend-mode wgrep exec-path-from-shell flycheck projectile yasnippet which-key nyan-mode multiple-cursors magit expand-region counsel company-quickhelp company beacon aggressive-indent ace-window zenburn-theme use-package)))
  '(projectile-mode t nil (projectile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
