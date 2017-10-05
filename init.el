df0062acc476e173e40b5f7985a3cb092e7a42ae
Christabella Irwanto <christabella.irwanto@gmail.com>
2017-10-05 09:27:36 +0800

    Add more golang config according to https://johnsogg.github.io/emacs-golang

--- init.el
+++ init.el
@@ -331,9 +331,44 @@
 
 ;; ---------------------------------------- Go ----------------------------------
 
-(require 'go-autocomplete)
 (require 'auto-complete-config)
 (ac-config-default)
+
+;; Define function to call when go-mode loads
+(defun my-go-mode-hook ()
+  (add-hook 'before-save-hook 'gofmt-before-save) ; gofmt before every save
+  (setq gofmt-command "goimports")                ; gofmt uses invokes goimports
+  (if (not (string-match "go" compile-command))   ; set compile command default
+      (set (make-local-variable 'compile-command)
+           "go build -v; and go test -v; and go vet"))
+
+  ;; guru settings
+  (go-guru-hl-identifier-mode)                    ; highlight identifiers
+  
+  ;; Key bindings specific to go-mode
+  (local-set-key (kbd "M-.") 'godef-jump)         ; Go to definition
+  (local-set-key (kbd "M-*") 'pop-tag-mark)       ; Return from whence you came
+  (local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
+  (local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
+  (local-set-key (kbd "M-]") 'next-error)         ; Go to next error (or msg)
+  (local-set-key (kbd "M-[") 'previous-error)     ; Go to previous error or msg
+
+  (electric-pair-mode)
+  (electric-indent-mode)
+  (flycheck-mode)
+  
+  ;; Misc go stuff
+  (auto-complete-mode 1))                         ; Enable auto-complete mode
+
+;; Connect go-mode-hook with the function we just defined
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+;; Ensure the go specific autocomplete is active in go-mode.
+(with-eval-after-load 'go-mode
+  (require 'go-autocomplete))
+
+;; If the go-guru.el file is in the load path, this will load it.
+(require 'go-guru)
 
 ;; -------------------------------------- React ----------------------------------
 (defun jethro/setup-rjsx-mode ()  
