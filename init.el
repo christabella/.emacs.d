b72d86dc4283a3d2724c5bafd0ae76d8c09cd86d
Jethro Kuan <jethrokuan95@gmail.com>
2017-10-14 00:45:12 +0800

    use https for package archives

--- init.el
+++ init.el
@@ -4,8 +4,8 @@
 ;; Enable package archives
 (when (>= emacs-major-version 24)
   (require 'package)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
   (add-to-list 'load-path "~/.emacs.d/lisp/")
   (package-initialize)
   )
