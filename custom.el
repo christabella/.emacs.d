43903321375518f8651eb356ab56b0f8aaaad515
Christabella Irwanto <christabella.irwanto@gmail.com>
2017-12-20 12:49:18 +0800

    Installed fish-mode

--- custom.el
+++ custom.el
@@ -3,25 +3,59 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
  '(c-basic-offset 4)
  '(crux-reopen-as-root-mode t)
  '(custom-safe-themes
    (quote
-    ("b81bfd85aed18e4341dbf4d461ed42d75ec78820a60ce86730fc17fc949389b2" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "5900bec889f57284356b8216a68580bfa6ece73a6767dfd60196e56d050619bc" default)))
+    ("ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "b81bfd85aed18e4341dbf4d461ed42d75ec78820a60ce86730fc17fc949389b2" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "5900bec889f57284356b8216a68580bfa6ece73a6767dfd60196e56d050619bc" default)))
  '(debug-on-error t)
  '(electric-indent-mode nil)
  '(electric-pair-mode t)
+ '(fci-rule-color "#556873")
  '(global-aggressive-indent-mode t)
- '(golden-ratio-mode t)
+ '(golden-ratio-mode nil)
+ '(jdee-db-active-breakpoint-face-colors (cons "#0d0f11" "#7FC1CA"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#0d0f11" "#A8CE93"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#0d0f11" "#899BA6"))
  '(nrepl-message-colors
    (quote
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(ns-use-native-fullscreen t)
+ '(org-fontify-done-headline t)
+ '(org-fontify-quote-and-verse-blocks t)
+ '(org-fontify-whole-heading-line t)
  '(package-selected-packages
    (quote
-    (company-go org-ref direnv json-mode dockerfile-mode org-download org-attach-screenshot flymake-python-pyflakes flycheck-pyflakes corral hydra move-text flymake-go protobuf-mode org-journal counsel-projectile auto-complete go-mode neotree emmet-mode tern rjsx-mode ranger dired-ranger doom-themes peep-dired emojify all-the-icons-dired unicode-fonts font-utils org-plus-contrib dired-details+ markdown-mode+ anaconda-mode crux isend-mode wgrep exec-path-from-shell flycheck projectile yasnippet which-key nyan-mode multiple-cursors magit expand-region counsel company-quickhelp company beacon aggressive-indent ace-window zenburn-theme use-package)))
+    (fish-mode flycheck-mode dumb-jump lua-mode yapfify py-isort golden-ratio dracula-theme company-go org-ref direnv json-mode dockerfile-mode org-download org-attach-screenshot flymake-python-pyflakes flycheck-pyflakes corral hydra move-text flymake-go protobuf-mode org-journal counsel-projectile auto-complete go-mode neotree emmet-mode tern rjsx-mode ranger dired-ranger doom-themes peep-dired emojify all-the-icons-dired unicode-fonts font-utils org-plus-contrib dired-details+ markdown-mode+ anaconda-mode crux isend-mode wgrep exec-path-from-shell flycheck projectile yasnippet which-key nyan-mode multiple-cursors magit expand-region counsel company-quickhelp company beacon aggressive-indent ace-window zenburn-theme use-package)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
- '(projectile-mode t nil (projectile)))
+ '(projectile-mode t nil (projectile))
+ '(vc-annotate-background "#0d0f11")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#A8CE93")
+    (cons 40 "#b8d293")
+    (cons 60 "#c9d693")
+    (cons 80 "#DADA93")
+    (cons 100 "#e2d291")
+    (cons 120 "#eaca90")
+    (cons 140 "#F2C38F")
+    (cons 160 "#e4bea4")
+    (cons 180 "#d6b9b9")
+    (cons 200 "#c9b4cf")
+    (cons 220 "#d0a6b8")
+    (cons 240 "#d799a2")
+    (cons 260 "#DF8C8C")
+    (cons 280 "#c98f92")
+    (cons 300 "#b39399")
+    (cons 320 "#9e979f")
+    (cons 340 "#556873")
+    (cons 360 "#556873")))
+ '(vc-annotate-very-old-color nil)
+ '(yas-global-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
