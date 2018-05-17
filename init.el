7e404020cfdf326166660037f4066b0e4e5380d5
Christabella Irwanto <christabella.irwanto@gmail.com>
2018-05-17 11:25:02 +0800

    Modify kbd's for dumb-jump

--- init.el
+++ init.el
@@ -473,7 +473,8 @@
 ;; ------------------------------------ Dumb jump --------------------------------
 (use-package dumb-jump
   :bind (("M-g o" . dumb-jump-go-other-window)
-         ("M-," . dumb-jump-go)
+         ("M-." . dumb-jump-go)
+         ("M-," . dumb-jump-back)
          ("M-g i" . dumb-jump-go-prompt)
          ("M-g x" . dumb-jump-go-prefer-external)
          ("M-g z" . dumb-jump-go-prefer-external-other-window))
