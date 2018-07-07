6f647a1e7462d1012ffa333b28c379871a590239
Christabella Irwanto <christabella.irwanto@gmail.com>
2018-07-07 12:23:09 +0200

    Magnify inline latex fragments in org-mode+transparent bg/default fg

--- custom.el
+++ custom.el
@@ -5,13 +5,17 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("4597d1e9bbf1db2c11d7cf9a70204fa42ffc603a2ba5d80c504ca07b3e903770" "723e48296d0fc6e030c7306c740c42685d672fd22337bc84392a1cf92064788a" "53d1bb57dadafbdebb5fbd1a57c2d53d2b4db617f3e0e05849e78a4f78df3a1b" "891debfe489c769383717cc7d0020244a8d62ce6f076b2c42dd1465b7c94204d" "c5d320f0b5b354b2be511882fc90def1d32ac5d38cccc8c68eab60a62d1621f2" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "aa0a998c0aa672156f19a1e1a3fb212cdc10338fb50063332a0df1646eb5dfea" "5715d3b4b071d33af95e9ded99a450aad674e308abb06442a094652a33507cd2" "3fb5d37cdafa4aa86eeb63fdbf3a82c446d90204429c9886393e7e9261a1e3dd" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default)))
+    ("242ed4611e9e78142f160e9a54d7e108750e973064cee4505bfcfc22cc7c61b1" "013c62a1fcee7c8988c831027b1c38ae215f99722911b69e570f21fc19cb662e" "4597d1e9bbf1db2c11d7cf9a70204fa42ffc603a2ba5d80c504ca07b3e903770" "723e48296d0fc6e030c7306c740c42685d672fd22337bc84392a1cf92064788a" "53d1bb57dadafbdebb5fbd1a57c2d53d2b4db617f3e0e05849e78a4f78df3a1b" "891debfe489c769383717cc7d0020244a8d62ce6f076b2c42dd1465b7c94204d" "c5d320f0b5b354b2be511882fc90def1d32ac5d38cccc8c68eab60a62d1621f2" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "aa0a998c0aa672156f19a1e1a3fb212cdc10338fb50063332a0df1646eb5dfea" "5715d3b4b071d33af95e9ded99a450aad674e308abb06442a094652a33507cd2" "3fb5d37cdafa4aa86eeb63fdbf3a82c446d90204429c9886393e7e9261a1e3dd" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default)))
  '(debug-on-error t)
  '(emojify-display-style (quote image))
  '(jdee-db-active-breakpoint-face-colors (cons "#191C25" "#80A0C2"))
  '(jdee-db-requested-breakpoint-face-colors (cons "#191C25" "#A2BF8A"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#191C25" "#434C5E"))
  '(magit-dispatch-arguments nil)
+ '(org-format-latex-options
+   (quote
+    (:foreground default :background "Transparent" :scale 1.6 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
+		 ("begin" "$1" "$" "$$" "\\(" "\\["))))
  '(org-hide-emphasis-markers t)
  '(org-indent-indentation-per-level 1)
  '(package-selected-packages
