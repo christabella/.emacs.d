3dc794a001b314bd96760ef07b338c49b0d47dd4
Christabella Irwanto <christabella.irwanto@gmail.com>
2018-09-20 13:38:30 +0300

    Save active buffer whenever focusing out of the frame

--- init.el
+++ init.el
@@ -13,6 +13,9 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
+;; Save on frame focus loss
+(add-hook 'focus-out-hook 'save-buffer)
+
 (setq-default explicit-shell-file-name "/usr/local/bin/fish")
 (setq-default shell-file-name "/usr/local/bin/fish")
 
